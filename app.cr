8f068ea902544361f5c80e73b387ccea75ad053f
Fatih Kadir Akın <fakin@protel.com.tr>
2016-03-10 23:29:36 +0200

    Get total search count from another api call

--- app.cr
+++ app.cr
@@ -7,6 +7,7 @@ require "./models/time_cache"
 
 SORT_OPTIONS = {"stars", "updated", "forks"}
 REPOS_CACHE = TimeCache(String, GithubRepos).new(30.minutes)
+ALL_REPOS_CACHE = TimeCache(String, GithubRepos).new(30.minutes)
 POPULAR_CACHE = TimeCache(String, GithubRepos).new(30.minutes)
 RECENTLY_CACHE = TimeCache(String, GithubRepos).new(30.minutes)
 
@@ -24,13 +25,6 @@ def crystal_repos(word = "", sort = "stars", page = 1, limit = 100)
   GithubRepos.from_json(response.body)
 end
 
-def filter(repos, filter)
-  filtered = repos.dup
-  filtered.items.select! { |item| matches_filter?(item, filter) }
-  filtered.total_count = filtered.items.size
-  filtered
-end
-
 def fetch_sort(env)
   env.params.query["sort"]?.try(&.to_s) || ""
 end
@@ -43,11 +37,6 @@ def fetch_page(env)
   env.params.query["page"]?.try(&.to_i) || 0
 end
 
-private def matches_filter?(item : GithubRepo, filter : String)
-  item.name.downcase.includes?(filter) ||
-    item.description.try(&.downcase.includes? filter)
-end
-
 get "/" do |env|
   sort = fetch_sort(env)
   filter = fetch_filter(env)
@@ -55,10 +44,11 @@ get "/" do |env|
   env.response.content_type = "text/html"
 
   repos = REPOS_CACHE.fetch(filter + "_" + sort + "_" + page.to_s) { crystal_repos(filter, sort, page) }
+  all_repos = ALL_REPOS_CACHE.fetch("all") { crystal_repos("", sort, page) }
   popular = POPULAR_CACHE.fetch(sort) { crystal_repos("", :stars, 1, 8) }
   recently = RECENTLY_CACHE.fetch(sort) { crystal_repos("", :updated, 1, 6) }
-  total = repos.not_nil!.total_count
-  repos = filter(repos, filter) unless filter.empty?
+
+  total = all_repos.not_nil!.total_count
   Views::Index.new total, repos, popular, recently, sort, filter, page
 end
 
