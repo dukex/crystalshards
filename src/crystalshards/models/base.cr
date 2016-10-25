df22a62b73c1c229ca6a196c81f0f229488c831b
Duke <dukekhaos@gmail.com>
2016-10-25 12:46:43 -0200

    improve base model

--- src/crystalshards/models/base.cr
+++ src/crystalshards/models/base.cr
@@ -1,33 +1,49 @@
 require "./connection"
 
-class Base
-  include Connection
-  extend Connection
-
-  def self.many(defintion, query)
-    puts "QUERY: #{query}"
-    connection.exec(defintion, query).to_hash.map { |r| mapping r }
+module CrystalShards
+  class NotFound < Exception
   end
 
-  def self.many(defintion, query, values)
-    puts "QUERY: #{query} (#{values})"
-    connection.exec(defintion, query, values).to_hash.map { |r| mapping r }
-  end
+  class Base
+    include Connection
+    extend Connection
 
-  def self.one(defintion, query, id)
-    puts "QUERY: #{query} [#{id}]"
-    mapping connection.exec(defintion, query, [id]).to_hash.first
-  end
+    def self.many(defintion, query)
+      puts "QUERY: #{query}"
+      connection.exec(defintion, query).to_hash.map { |r| mapping r }
+    end
 
-  def self.ensure_time(value) : Time
-    value.is_a?(Time) ? value : raise "#{value} is not a Time"
-  end
+    def self.many(defintion, query, values)
+      puts "QUERY: #{query} (#{values})"
+      connection.exec(defintion, query, values).to_hash.map { |r| mapping r }
+    end
 
-  def self.ensure_string(value) : String
-    value.is_a?(String) ? value : raise "#{value} is not a String"
-  end
+    def self.one(defintion, query, id)
+      puts "QUERY: #{query} [#{id}]"
+      results = connection.exec(defintion, query, [id]).to_hash
+      begin
+        results.first
+      rescue IndexError
+        raise NotFound.new("Not found register with query #{query} [#{id}]")
+      end
 
-  def self.ensure_integer(value) : Int32
-    value.is_a?(Int32) ? value : raise "#{value} is not a Int32"
+      mapping results.first
+    end
+
+    def self.ensure_time(value) : Time
+      value.is_a?(Time) ? value : raise "#{value} is not a Time"
+    end
+
+    def self.ensure_string(value) : String
+      value.is_a?(String) ? value : raise "#{value} is not a String"
+    end
+
+    def self.ensure_integer(value) : Int32
+      value.is_a?(Int32) ? value : raise "#{value} is not a Int32"
+    end
+
+    def self.ensure_bool(value) : Bool
+      value.is_a?(Bool) ? value : raise "#{value} is not a Boolean"
+    end
   end
 end
